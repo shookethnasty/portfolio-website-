bd1cb7411a376d6774fd7d07e79fc8704ca00fba
shookethnasty <130115002+shookethnasty@users.noreply.github.com>
2026-01-20 14:15:55 +1100

    removed the blur blob effect

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,35 +1,37 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
+import Work from "./components/Work/Work";
+import Education from "./components/Education/Education";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import BlurBlob from "./components/BlurBlob";
 
-import react from 'react'
-import Navbar from './components/Navbar/Navbar.jsx'
-import About from './components/About/about.jsx'
-import Education from './components/Education/Education.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import Skills from './components/Skills/Skills.jsx'
-import Footer from './components/Footer/Footer.jsx'
-import Work from './components/Work/Work.jsx'
-import Experience from './components/Experience/Experience.jsx'
-
-
-
-function App() {
-
+const App = () => {
   return (
-    <div className="bg-[#050414]">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-        
-        <div className="relative pt-20">
-          <Navbar />
-          <About />
-          <Skills />
-          <Experience />
-          <Work />
-          <Education />
-          <Contact />
-          <Footer />
-        </div>
+    <div className="min-h-screen bg-[#050414] relative overflow-hidden">
+      {/* Ambient blobs */}
+      <BlurBlob position={{ top: "35%", left: "20%" }} size={{ width: "30%", height: "40%" }} />
+      <BlurBlob position={{ top: "70%", left: "80%" }} size={{ width: "28%", height: "36%" }} />
 
+      {/* Grid overlay */}
+      <div className="pointer-events-none absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_55%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
+      {/* Content */}
+      <div className="relative z-10 pt-20">
+        <Navbar />
+        <About />
+        <Skills />
+        <Experience />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
